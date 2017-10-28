ebaff7a38e11fb47f9f21108e4c1f13f2b91382d
Rickard Jonsson <me@rickardjonsson.se>
2017-10-28 15:07:59 +0200

    Temporary test structure
    Added temporary way to test functionality until test library is
    introduced.

--- src/arse.cpp
+++ src/arse.cpp
@@ -3,71 +3,79 @@
 
 #include "le.hpp"
 
+#include "testing.hpp"
+
+void exec_command(le *editor, char mode);
 int main(int argc, char **argv){
     if(argc < 2){
-        std::cout << "Give me a file yo" << std::endl;
-        return 1;
-    }
-    FILE *fp = fopen(argv[1], "r");
-    if(fp){
-        std::string input;
-        fseek(fp, 0, SEEK_END);
-        int file_len = std::ftell(fp);
-        fseek(fp, 0, SEEK_SET);
-        input.resize(file_len);
-        fread(&input[0], 1, file_len, fp);
-        le *editor = new le(input);
-        if(argc > 2){
+        le *editor = new le("Testing");
+        std::cout << "Insertion test" << std::endl;
+        editor->insert(4, "<SOMETHING>");
+        assert(editor->get_buffer(), "Test<SOMETHING>ing");
+        editor->insert(0, "<Here>");
+        assert(editor->get_buffer(), "<Here>Test<SOMETHING>ing");
+        editor->insert(2, "<Hello>");
+        assert(editor->get_buffer(), "<H<Hello>ere>Test<SOMETHING>ing");
+        editor->insert(4, "<Last one>");
+        assert(editor->get_buffer(), "<H<H<Last one>ello>ere>Test<SOMETHING>ing");
+        editor->insert(11, "<Kidding>");
+        assert(editor->get_buffer(), "<H<H<Last o<Kidding>ne>ello>ere>Test<SOMETHING>ing");
+        /* Undo/redo and removal causes memory leaks */
+        std::cout << "Undo/redo test" << std::endl;
+        editor->undo();
+        assert(editor->get_buffer(), "<H<H<Last one>ello>ere>Test<SOMETHING>ing");
+        editor->undo();
+        assert(editor->get_buffer(), "<H<Hello>ere>Test<SOMETHING>ing");
+        editor->redo();
+        assert(editor->get_buffer(), "<H<H<Last one>ello>ere>Test<SOMETHING>ing");
+        editor->undo();
+        assert(editor->get_buffer(), "<H<Hello>ere>Test<SOMETHING>ing");
+        std::cout << "Removal test" << std::endl;
+        /* Delete across pieces */
+        editor->remove(4, 10);
+        assert(editor->get_buffer(), "<H<Hest<SOMETHING>ing");
+        /* Delete whole piece */
+        editor->remove(0, 2);
+        assert(editor->get_buffer(), "<Hest<SOMETHING>ing");
+        /* Delete in same piece */
+        editor->remove(6, 0);
+        assert(editor->get_buffer(), "<Hest<SOMETHING>ing");
+        delete editor;
+    } else {
+        FILE *fp = fopen(argv[1], "r");
+        if(fp){
+            std::string input;
+            fseek(fp, 0, SEEK_END);
+            int file_len = std::ftell(fp);
+            fseek(fp, 0, SEEK_SET);
+            input.resize(file_len);
+            fread(&input[0], 1, file_len, fp);
+            le *editor = new le(input);
             char mode;
             do{
                 std::cin >> mode;
-                if(mode == 'i'){
-                    int index;
-                    std::string text;
-                    std::cin >> index;
-                    std::cin >> text;
-                    editor->insert(index, text);
-                }
-                if(mode == 'u'){
-                    editor->undo();
-                }
+                exec_command(editor, mode);
                 std::cout << editor->get_buffer() << std::endl;
             } while(mode != 'q');
 
-        } else {
-            std::cout << "Insertion test" << std::endl;
-            editor->insert(4, "<SOMETHING>");
-            std::cout << editor->get_buffer() << std::endl;
-            editor->insert(0, "<Here>");
-            std::cout << editor->get_buffer() << std::endl;
-            editor->insert(2, "<Hello>");
-            std::cout << editor->get_buffer() << std::endl;
-            editor->insert(4, "<Last one>");
-            std::cout << editor->get_buffer() << std::endl;
-            editor->insert(11, "<Kidding>");
-            std::cout << editor->get_buffer() << std::endl;
-            /* Undo/redo and removal causes memory leaks */
-            std::cout << "Undo/redo test" << std::endl;
-            editor->undo();
-            std::cout << editor->get_buffer() << std::endl;
-            editor->undo();
-            std::cout << editor->get_buffer() << std::endl;
-            editor->redo();
-            std::cout << editor->get_buffer() << std::endl;
-            editor->undo();
-            std::cout << editor->get_buffer() << std::endl;
-            std::cout << "Removal test" << std::endl;
-            /* Delete across pieces */
-            editor->remove(4, 10);
-            std::cout << editor->get_buffer() << std::endl;
-            /* Delete whole piece */
-            editor->remove(0, 2);
-            std::cout << editor->get_buffer() << std::endl;
-            /* Delete in same piece */
-            editor->remove(6, 0);
-            std::cout << editor->get_buffer() << std::endl;
-        }
-        delete editor;
+            delete editor;
     }
     fclose(fp);
+    }
+}
+void exec_command(le *editor, char mode){
+    int index;
+    std::string text;
+    switch(mode){
+        case 'i':
+            std::cin >> index;
+            std::cin >> text;
+            editor->insert(index, text);
+            break;
+        case 'u':
+            editor->undo();
+            break;
+        default:
+            std::cout << "Not a command" << std::endl;
+    }
 }
