0d338551442f8ef8751a50b74293f87d806b5f40
Rickard Jonsson <me@rickardjonsson.se>
2018-04-27 17:01:21 +0200

    Fixed function declaration variable names

--- src/arse.h
+++ src/arse.h
@@ -12,16 +12,16 @@ struct arse{
   FILE *fp;
 };
 struct arse *arse_create(char *str, int file);
-void arse_delete(struct arse *t);
-int arse_insert(struct arse *t, size_t index, char *str);
-int arse_insert_at_line(struct arse *t, size_t line, size_t index, char *str);
-int arse_remove(struct arse *t, size_t index, size_t length);
-void arse_undo(struct arse *t);
-void arse_redo(struct arse *t);
-void arse_undo_line(struct arse *t, size_t line);
-void arse_redo_line(struct arse *t, size_t line);
-char *arse_get_line(struct arse *t, size_t line);
-int arse_save(struct arse *t);
-void arse_backup(struct arse *t);
-char *arse_buffer(struct arse *t);
+void arse_delete(struct arse *a);
+int arse_insert(struct arse *a, size_t index, char *str);
+int arse_insert_at_line(struct arse *a, size_t line, size_t index, char *str);
+int arse_remove(struct arse *a, size_t index, size_t length);
+void arse_undo(struct arse *a);
+void arse_redo(struct arse *a);
+void arse_undo_line(struct arse *a, size_t line);
+void arse_redo_line(struct arse *a, size_t line);
+char *arse_get_line(struct arse *a, size_t line);
+int arse_save(struct arse *a);
+void arse_backup(struct arse *a);
+char *arse_buffer(struct arse *a);
 #endif /* ARSE_H */
