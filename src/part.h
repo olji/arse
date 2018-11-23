26bc16327431cb1152f011787436048762c70f21
Rickard Jonsson <me@rickardjonsson.se>
2018-11-23 16:16:10 +0100

    Cosmetic newline

--- src/part.h
+++ src/part.h
@@ -5,6 +5,7 @@ struct part{
   struct piece *first;
   struct piece *last;
 };
+
 struct part *part_create(struct piece *first, struct piece *last, int deepcopy);
 void part_delete(struct part *p, int deep);
 int part_length(struct part *p);
