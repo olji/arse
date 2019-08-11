68375eb8007e428353b541974d6f493431f64cd5
Rickard Jonsson <me@rickardjonsson.se>
2019-08-11 23:39:27 +0200

    Added some function documentation

--- src/arse.h
+++ src/arse.h
@@ -20,15 +20,21 @@ struct arse_config{
   /* Directory to put backup files */
   char *bkup_dir;
 };
+
+/// @brief A string with length supplied
 struct arse_string{
   char *string;
   size_t length;
 };
+
+/// @brief All lines handled by arse instance with meta-information
 struct arse_buffer{
   char **lines;
   size_t *line_lengths;
   size_t lines_count;
 };
+
+/// @brief Main editor and sub-editor structure
 struct arse{
   /* TODO: some structure for keeping track of extra lines inside subarses */
   struct table **lines;
@@ -46,26 +52,168 @@ struct arse{
   FILE *fp;
 };
 
+/// @brief Initialise an arse instance at supplied memory space
+///
+/// @param a Memory address to initialise
 void arse_init(struct arse *a);
+
+/// @brief Open and load content from file into arse instance
+///
+/// @param a Arse instance
+/// @param path Path to file
+///
+/// @return Zero on success, non-zero on failure
 int arse_open_file(struct arse *a, char *path);
+
+/// @brief Load arse instance with a basic string
+///
+/// @param a Arse instance
+/// @param str Input string
 void arse_load_string(struct arse *a, char *str);
+
+/// @brief Clean up memory space of arse instance
+///
+/// @param a Arse instance
 void arse_delete(struct arse *a);
+
+/// @brief Insert string at index
+///
+/// @param a Arse instance
+/// @param index Character index to start insertion at
+/// @param str String to insert
+///
+/// @return Zero on success, non-zero on failure
 int arse_insert(struct arse *a, size_t index, char *str);
+
+/// @brief Insert string at index on line
+///
+/// @param a Arse instance
+/// @param line Line to insert in
+/// @param index Character index at specificed line
+/// @param str String to insert
+///
+/// @return Zero on success, non-zero on failure
 int arse_insert_at_line(struct arse *a, size_t line, size_t index, char *str);
+
+/// @brief Remove continuous sequence of character
+///
+/// @param a Arse instance
+/// @param index Index to start removal from
+/// @param length Length of sequence
+///
+/// @return Zero on success, non-zero on failure
 int arse_remove(struct arse *a, size_t index, size_t length);
+
+/// @brief Remove continuous sequence of character at line
+///
+/// @param a Arse instance
+/// @param line Line to remove from
+/// @param index Index to start removal from at specified line
+/// @param length Length of sequence
+///
+/// @return Zero on success, non-zero on failure
 int arse_remove_at_line(struct arse *a, size_t line, size_t index, size_t length);
+
+/// @brief Create a new, empty line
+///
+/// @param a Arse instance
+/// @param line Position to add line at
 void arse_new_line(struct arse *a, size_t line);
+
+/// @brief Undo last action
+///
+/// This will remove the last action of the most recently modified
+/// table
+///
+/// @param a Arse instance
 void arse_undo(struct arse *a);
+
+/// @brief Get absolute index (starting at 0 from first line)
+///
+/// @param a Arse instance
+/// @param line Line which index is relative to
+/// @param index Relative index to line
+///
+/// @return Absolute index
 size_t arse_index_for_line(struct arse *a, size_t line, size_t index);
+
+/// @brief Redo last action
+///
+/// This will redo the last undone action of the most recently
+/// undone table
+///
+/// @param a Arse instance
 void arse_redo(struct arse *a);
+
+/// @brief Undo specific line
+///
+/// Undos the last action on specific line
+///
+/// @param a Arse instance
+/// @param line Line to undo
 void arse_undo_line(struct arse *a, size_t line);
+
+/// @brief Redo specific line
+///
+/// Redo the last undone action on specific line
+///
+/// @param a Arse instance
+/// @param line Line to redo
 void arse_redo_line(struct arse *a, size_t line);
+
+/// @brief Get string of specific line
+///
+/// @param a Arse instance
+/// @param line Line to fetch string from
+///
+/// @return String found at given line index
 char *arse_get_line(struct arse *a, size_t line);
-int arse_piece_to_arse(struct arse *a, size_t line, size_t index, size_t length, bool force);
+
+/// @brief Convert piece sequence for text sequence into a sub-editor arse instance
+///
+/// @param a Main arse instance
+/// @param line Line index of table to modify
+/// @param index Text index start point of conversion
+/// @param length Length of text sequence
+/// @param unique Force creation of new instance if duplicate is found, instead of sharing same instance
+///
+/// @return 
+int arse_piece_to_arse(struct arse *a, size_t line, size_t index, size_t length, bool unique);
+
+/// @brief Write buffer to file
+///
+/// @param a Arse instance
+/// @param filename Path to target file
+///
+/// @return Zero on success
 int arse_save(struct arse *a, char *filename);
+
+/// @brief Save backup file
+///
+/// @param a Arse instance
 void arse_backup(struct arse *a);
+
+/// @brief Clean up arse buffer structure
+///
+/// @param a Arse buffer instance
 void arse_buffer_delete(struct arse_buffer *a);
+
+/// @brief Clean up arse string structure
+///
+/// @param a Arse string instance
 void arse_string_delete(struct arse_string *a);
+
+/// @brief Get buffer information of arse instance
+///
+/// @param a Arse instance
+///
+/// @return Arse buffer structure containing buffer information
 struct arse_buffer *arse_get_buffer(struct arse *a);
+
+/// @brief Get plain string managed by arse instance
+///
+/// @param a Arse instance
+///
+/// @return Current string handled by arse instance
 struct arse_string *arse_get_string(struct arse *a);
 #endif /* ARSE_H */
--- src/table.h
+++ src/table.h
@@ -9,12 +9,15 @@ struct part_stack;
 struct part;
 struct piece;
 
+/// @brief Basic meta-information of contents in table
 struct table_info{
   size_t line_count;
   size_t *line_lengths;
   size_t arse_count;
   struct piece **arse_nodes;
 };
+
+/// @brief Piece table structure with undo/redo and text information
 struct table{
   struct piece *begin;
   struct piece *end;
@@ -26,20 +29,108 @@ struct table{
   struct part_stack *future;
 };
 
+/// @brief Create a instance of table
+///
+/// @param input Initial string which instance will represent
+/// @param static_str If true it will not try to clean up memory of the input string
+///
+/// @return Newly created instance of table
 struct table *table_create(char *input, bool static_str);
+
+/// @brief Clean up memory used by table instance 
+///
+/// @param t Instance to clean up
 void table_delete(struct table *t, int free_input);
+
+/// @brief Insert text into piece chain
+///
+/// @param t Table instance
+/// @param index Index to start insertion
+/// @param str String to insert
+///
+/// @return Unused, always NULL
 struct piece *table_insert(struct table *t, size_t index, char *str);
+struct piece *table_insert_line(struct table *t, size_t index, char *str);
+
+/// @brief Remove continuous text section from piece chain
+///
+/// @param t Table instance
+/// @param from Index of first character to remove
+/// @param length Amount of characters to remove in total
 void table_remove(struct table *t, size_t from, size_t length);
+
+/// @brief Undo last operation
+///
+/// @param t Table instance
 void table_undo(struct table *t);
+
+/// @brief Redo last undone operation
+///
+/// @param t Table instance
 void table_redo(struct table *t);
+
 struct table *table_split(struct table *t, size_t index);
+
 size_t table_length(struct table *t);
+
+/// @brief Get string which table instance is representing
+///
+/// @param t Table instance
+///
+/// @return Text buffer constructed from piece chain
 char *table_buffer(struct table *t);
+
+/// @brief DEBUG METHOD: prints series of pieces from piece chain
+///
+/// @param t Table instance
+/// @param begin Start piece
+/// @param end End piece
 void table_print_series(struct table *t, struct piece *begin, struct piece *end);
-int table_state_update(struct table *t);
+
+/// @brief DEBUG METHOD: Print piece information from piece chain
+///
+/// @param t Table instance
+/// @param p Piece
 void table_print_piece(struct table *t, struct piece *p);
+
+/// @brief Update table_info member in table instance
+///
+/// @param t Table instance
+///
+/// @return Zero if finished without errors
+int table_state_update(struct table *t);
+
+/// @brief Get container of a section of piece chain
+///
+/// @param t Table instance
+/// @param from Index to start container from, split piece if necessary
+/// @param length Length to cover, split piece if necessary
+///
+/// @return Part instance which covers the range specified
 struct part *table_get_pieces(struct table *t, size_t from, size_t length);
+
+/// @brief Get representation of pieces in piece chain section
+///
+/// @param t Table instance
+/// @param start Beginning piece
+/// @param end Last piece
+/// @param result Char array to write results to
 void table_get_string(struct table *t, struct piece *start, struct piece *end, char *result);
+
+/// @brief Replace series of pieces with one piece instance
+///
+/// @param t Table instance
+/// @param start Start of piece series to be replaced
+/// @param end End of piece series to be replaced
+/// @param replacement Piece to replace piece series with
 void table_replace_pieces(struct table *t, struct piece *start, struct piece *end, struct piece *replacement);
+
+/// @brief Turn piece containing index character to an arse instance
+///
+/// @param t Table instance
+/// @param index Index of string of the piece chain
+///
+/// @return 
 int table_piece_to_arse(struct table *t, size_t index);
+
 #endif /* TABLE_H */
