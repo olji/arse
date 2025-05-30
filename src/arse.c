217b237906503a89235be1feaa047a42e2bb5acb
Rickard Jonsson <rickard@rickardjonsson.se>
2025-05-30 02:08:10 +0200

    Start changing usage of arse struct
    Remove string-manipulation from the editor, as such file i/o is removed.
    
    Primary use-case is meant for representing one line in an higher level
    text editor that manages multiple arse instances to support multi-line
    editing, and can tie together sub-editors between lines that way.
    
    Original string content is not its concern really, so don't handle
    that in memory. Higher level text editor is more suitable for that.
    
    Addition buffer is left (for now), maybe that's more suitable to be
    managed by higher level text editor as well.

--- src/arse.c
+++ src/arse.c
@@ -26,65 +26,41 @@ size_t default_hashfunc(char *str, size_t mod){
   return pos % mod;
 }
 void arse_init(struct arse *a){
-  a->fp = NULL;
-  a->masters = NULL;
-  a->hosts = NULL;
-  a->masters_count = 0;
-  a->hosts_count = 0;
-  a->action_history = table_stack_create();
-  a->action_future = table_stack_create();
-  a->lines_count = 0;
-  a->absolute_lines_count = a->lines_count;
-  a->slaves = subarse_table_create(16, &default_hashfunc);
+  //a->action_history = table_stack_create();
+  //a->action_future = table_stack_create();
+  //a->slaves = subarse_table_create(16, &default_hashfunc);
+  a->buffers[ARSE_CHANGE] = calloc(1, sizeof(char));
+
+  a->begin = piece_create(0, 0, ARSE_NONE);
+  a->end = piece_create(0, 0, ARSE_NONE);
+
+  a->begin->previous = a->begin;
+  a->end->next = a->end;
+
 }
-int arse_open_file(struct arse *a, char *path){
-  /* TODO: Optimize file load to reuse repeated string (Conf option) */
-  size_t length = 0;
-  a->filename = path;
-  a->fp = fopen(path, "r+");
-  char *str;
-  if(a->fp){
-    fseek(a->fp, 0, SEEK_END);
-    length = ftell(a->fp);
-    fseek(a->fp, 0, SEEK_SET);
-    if(length == 0){
-      str = calloc(2, sizeof(char));
-      str[0] = 10;
-    } else {
-      str = calloc(length + 1, sizeof(char));
-      fread(&str[0], 1, length, a->fp);
-    }
-    fclose(a->fp);
-    a->fp = NULL;
-  } else {
-    return 1;
-  }
-  parse_input_string(a, str, length);
-  return 0;
-}
-void arse_load_string(struct arse *a, char *str){
-  parse_input_string(a, str, strlen(str));
+void arse_load_string(struct arse *a, char *initial_str){
+  parse_input_string(a, initial_str, strlen(initial_str));
 }
 void arse_delete(struct arse *a){
-  for(int i = 0; i < a->lines_count; ++i){
-    table_delete(a->lines[i], true);
-  }
-  table_stack_delete(a->action_history);
-  table_stack_delete(a->action_future);
-  subarse_table_delete(a->slaves);
-  free(a->lines);
-  if(a->masters != NULL){
-    free(a->masters);
-  }
-  if(a->hosts != NULL){
-    free(a->hosts);
-  }
-  if(a->fp != NULL){
-    fclose(a->fp);
-  }
+  //table_stack_delete(a->action_history);
+  //table_stack_delete(a->action_future);
+  //subarse_table_delete(a->slaves);
+  //if(a->hosts != NULL){
+  //free(a->hosts);
+  //}
+  struct piece *d = a->begin;
+  struct piece *next = d->next;
+  do{
+     piece_delete(d);
+     d = next;
+     next = next->next;
+  } while (d != a->end);
+  piece_delete(a->end);
+  free(a->buffers[ARSE_CHANGE]);
   free(a);
 }
 int arse_insert(struct arse *a, size_t index, char *str){
+  /*
   size_t table = 0;
   size_t length = 0;
   size_t insertions = strinst(str, '\n') + 1;
@@ -107,207 +83,121 @@ int arse_insert(struct arse *a, size_t index, char *str){
   } else {
     insertion_strings[0] = heap_str;
   }
-  while(table < a->lines_count - 1 && length + a->lines[table]->length <= index){
-    length += a->lines[table++]->length;
-  }
-  if(table >= a->lines_count){
-    return 1;
-  }
 
   index = index - length;
-  table_stack_clean_instance(a->action_future, a->lines[table]);
-  table_stack_push(a->action_history, a->lines[table]);
-  /* TODO: table_insert should return old string content if node affected was an editor so position in arsetable string key can be updated */
-  a->absolute_lines_count += insertions - 1;
-  if(insertions > 1){
-    /* Newline was in inserted string */
-    /* TODO: Clean up and refactor when code is working */
-    /* TODO: Shrink memory block of initial table */
-    /* TODO: Table lengths need to be updated */
-    /* table_remove(line, index, end), remove part created when inserting first insertion string */
-
-    char *table_last_string = calloc(a->lines[table]->length + 1, sizeof(char));
-    table_get_string(a->lines[table], a->lines[table]->begin, a->lines[table]->end, table_last_string);
-    /* Last newline has to be cropped here */
-    char *last_string = malloc(strlen(table_last_string) - index + strlen(insertion_strings[insertions-1]) + 1);
-    debug("Allocated space: %zu -- ins_string length: %zu -- ls_len: %zu\n", strlen(table_last_string) - index + strlen(insertion_strings[insertions-1]), strlen(insertion_strings[insertions-1]), strlen(table_last_string + index));
-    strcpy(last_string, insertion_strings[insertions-1]);
-    strcpy(last_string + strlen(insertion_strings[insertions-1]), table_last_string + index);
-    table_remove(a->lines[table], index, a->lines[table]->length - index);
-
-    for(int i = 1; i < insertions; ++i){
-      struct table *t = table_create("", true);
-      a->lines = table_array_insert_at(a->lines, t, table + i);
-      ++a->lines_count;
-    }
-
-    /* TODO: Fix memory leak of last_string */
-    insertion_strings[insertions-1] = last_string;
-
-    free(table_last_string);
-  }
-  for(int i = 0; i < insertions; ++i){
-    if(i > 0){
-      index = 0;
-    }
-    char *string = calloc(a->lines[table]->length + 1, sizeof(char));
-    table_get_string(a->lines[table], a->lines[table]->begin, a->lines[table]->end, string);
-    free(string);
-    /* TODO: Combine several insertions into one undo-action, rewrite history and future structures to contain part collections */
-    table_insert(a->lines[table + i], index, insertion_strings[i]);
-    if(insertions > 0 && i == 0){
-      /* TODO: Remove insertion action */
-    }
-    string = calloc(a->lines[table]->length + 1, sizeof(char));
-    table_get_string(a->lines[table], a->lines[table]->begin, a->lines[table]->end, string);
-    free(string);
-  }
-  free(insertion_strings);
-  free(heap_str);
   return 0;
-}
-int arse_insert_at_line(struct arse *a, size_t line, size_t index, char *str){
-  return arse_insert(a, arse_index_for_line(a, line, index), str);
+  */
+  struct piece *p = a->begin;
+  size_t distance = 0;
+  size_t length = strlen(str);
+  while(distance + p->length < index && p != a->end){
+    distance += p->length;
+    p = p->next;
+  }
+  if(p->buffer == ARSE_EDITOR){
+    int result = arse_insert(p->arse, index - distance, str);
+    if(result != 0){
+      debug("Failed inserting to subarse\n");
+      return NULL;
+    }
+    // TODO: Verify this works
+    /*
+    for(size_t i = 0; i < p->arse->masters_count; ++i){
+      p->arse->masters[i]->length += length;
+    }
+    */
+    //for(size_t i = 0; i < p->arse->hosts_count; ++i){
+      //p->arse->hosts[i]->length += length;
+    //}
+  } else {
+    size_t start = strlen(a->buffers[ARSE_CHANGE]);
+    struct piece *n = piece_create(start, length, ARSE_CHANGE);
+    char *tmp = realloc(a->buffers[ARSE_CHANGE], sizeof(char) * (start + length + 1));
+    if(tmp != NULL){
+      tmp[start+length] = 0;
+      a->buffers[ARSE_CHANGE] = tmp;
+    } else {
+      return NULL;
+    }
+    strcpy(a->buffers[ARSE_CHANGE] + start, str);
+    if(p != a->end){
+      index = index - distance;
+    } else {
+      index = p->length;
+    }
+    bool even = p->length == index;
+    /*
+    struct part *range;
+    if(even){
+      range = part_create(p, p->next, 1);
+    } else {
+      range = part_create(p, p, 1);
+    }
+    part_stack_push(a->history, range);
+    */
+
+    if(!even){
+      piece_split(p, index);
+    }
+    /* TODO: Some nicer way to handle when inserting at end of piece chain */
+    if(p != a->end){
+      piece_insert_after(p, n);
+    } else {
+      piece_insert_before(p, n);
+    }
+    p->length = index;
+  }
+  return 0;
 }
 int arse_remove(struct arse *a, size_t index, size_t length){
-  size_t table = 0;
-  size_t len = 0;
-  while(table < a->lines_count - 1 && len + a->lines[table]->length < index){
-    len += a->lines[table++]->length;
-  }
-  if(table >= a->lines_count){
-    debug("Failure\n");
-    return 1;
-  }
-  table_stack_clean_instance(a->action_future, a->lines[table]);
-  table_stack_push(a->action_history, a->lines[table]);
-  /* TODO: table_remove should return old string content if node affected was an editor so position in arsetable can be updated */
-  table_remove(a->lines[table], index - len, length);
   return 0;
 }
-int arse_remove_at_line(struct arse *a, size_t line, size_t index, size_t length){
-  return arse_remove(a, arse_index_for_line(a, line, index), length);
-}
-/* TODO: remove empty line as well */
-/* TODO: Include action in undo/redo */
-void arse_new_line(struct arse *a, size_t line){
-  struct table *t = table_create("\n", true);
-  a->lines = table_array_insert_at(a->lines, t, line);
-  ++a->lines_count;
-  ++a->absolute_lines_count;
-}
 void arse_undo(struct arse *a){
+  /*
   struct table *t = table_stack_pop(a->action_history);
   if(t != NULL){
     table_stack_push(a->action_future, t);
     table_undo(t);
   }
+  */
 }
 void arse_redo(struct arse *a){
+  /*
   struct table *t = table_stack_pop(a->action_future);
   if(t != NULL){
     table_stack_push(a->action_history, t);
     table_redo(t);
   }
-}
-/* TODO: Fix undo/redo for specific lines to work with tables with multiple newlines to do \n being in insertion, or just remove these functions */
-void arse_undo_line(struct arse *a, size_t line){
-  table_stack_pop_instance(a->action_history, a->lines[line]);
-  table_stack_push(a->action_future, a->lines[line]);
-  table_undo(a->lines[line]);
-}
-void arse_redo_line(struct arse *a, size_t line){
-  table_stack_push(a->action_history, a->lines[line]);
-  table_redo(a->lines[line]);
-}
-int arse_save(struct arse *a, char *filename){
-  FILE *fp = NULL;
-  /* TODO: Potentially use this in conjunction to detecting external file changes */
-  //if(strcmp(filename, a->filename) == 0){
-  //  fp = freopen(NULL, "w", a->fp);
-  //} else {
-  //  fp = fopen(filename, "w");
-  //  return 1;
-  //}
-  fp = fopen(filename, "w");
-  fseek(fp, 0, SEEK_SET);
-  struct arse_string *str = arse_get_string(a);
-  fwrite(str->string, str->length, 1, fp);
-  fflush(fp);
-  a->fp = freopen(NULL, "r+", fp);
-  return 0;
+  */
 }
 void arse_backup(struct arse *a){
 }
-char *arse_get_line(struct arse *a, size_t line){
-  return table_buffer(a->lines[line]);
-}
 struct arse_string *arse_get_string(struct arse *a){
-  char *buffer;
-  size_t total_length = 0;
-  for(int i = 0; i < a->lines_count; ++i){
-    total_length += a->lines[i]->length;
-    debug("total_length: %zu\n", total_length);
-  }
-  buffer = calloc(total_length + 1, sizeof(char));
-  size_t written_length = 0;
-  for(int i = 0; i < a->lines_count; ++i){
-    char *t_buffer = table_buffer(a->lines[i]);
-    strcpy(buffer + written_length, t_buffer);
-    debug("Adding %zu to written length.\n", strlen(t_buffer));
-    written_length += strlen(t_buffer);
-    free(t_buffer);
-    /* FIXME: This free causes segfaults, check it out when insertion with newline doesn't cause undo/redo to fail */
-    //free(t_buffer);
-  }
-  if(total_length != written_length){
-    debug("Something is off: %zu -- %zu\n", total_length, written_length);
-  }
-  //buffer[total_length] = 0;
-  struct arse_string *result = malloc(sizeof(struct arse_string));
-  result->string = buffer;
-  result->length = total_length;
-  return result;
-}
-struct arse_buffer *arse_get_buffer(struct arse *a){
-  struct arse_buffer *result = malloc(sizeof(struct arse_buffer));
-  result->lines = calloc(a->absolute_lines_count, sizeof(char*));
-  result->line_lengths = malloc(sizeof(size_t) * a->absolute_lines_count);
-  for(int line = 0, abs_line = 0; line < a->lines_count; ++line){
-    char *t_buffer = table_buffer(a->lines[line]);
-    //struct table_info state = a->lines[line]->state;
-    size_t copied = 0;
-    size_t end = strlen(t_buffer);
-    char *line_start = t_buffer;
-    while(copied < end){
-      char *line_end = strchr(line_start, '\n');
-      size_t len = 0;
-      if(line_end != NULL){
-        line_end += 1;
-        len = line_end - line_start;
-      } else {
-        len = strlen(line_start);
-      }
-      result->line_lengths[abs_line] = len;
-      result->lines[abs_line] = calloc(len + 1, sizeof(char));
-      if(line_end != NULL){
-        strncpy(result->lines[abs_line], line_start, len);
-      } else {
-        strncpy(result->lines[abs_line], line_start, strlen(line_start));
-      }
-      copied += line_end - line_start + 1;
-      line_start = line_end + 1;
-      ++abs_line;
+  size_t copy_index = 0;
+  struct piece *start = a->begin, *end = a->end;
+  size_t length = piece_chain_length(start, end);
+  char *str = calloc(length + 1, sizeof(char));
+
+  while(start != end->next){
+    if(start->buffer == ARSE_EDITOR){
+      struct arse_string *str = arse_get_string(start->arse);
+      strncpy(str + copy_index, str->string, str->length);
+      copy_index += str->length;
+      arse_string_delete(str);
+    } else {
+      strncpy(str + copy_index, a->buffers[start->buffer] + start->start, start->length);
+      copy_index += start->length;
     }
-    free(t_buffer);
+    start = start->next;
   }
-  result->lines_count = a->absolute_lines_count;
+  struct arse_string *result = malloc(sizeof(struct arse_string));
+  result->string = str;
+  result->length = length;
   return result;
 }
 static struct arse *arse_find_arse(struct arse *a, size_t line, size_t length, struct part *p){
-  char *string = calloc(length + 1, sizeof(char));
-  table_get_string(a->lines[line], p->first, p->last, string);
-  string[length] = 0;
+  // TODO
+  /*
   struct arse *ed = subarse_table_find(a->slaves, string);
   if(ed == NULL){
     ed = malloc(sizeof(struct arse));
@@ -318,20 +208,11 @@ static struct arse *arse_find_arse(struct arse *a, size_t line, size_t length, s
     free(string);
   }
   return ed;
+  */
+  return NULL;
 }
 static int arse_update_arrays(struct arse *a, struct arse *ed, size_t line){
-  if(ed->masters == NULL){
-    ed->masters = calloc(1, sizeof(struct table*));
-    ed->masters_count = 1;
-  } else {
-    struct table **tmp = realloc(ed->masters, ++ed->masters_count * sizeof(struct table*));
-    if(tmp != NULL){
-      ed->masters = tmp;
-    } else {
-      return 1;
-    }
-  }
-  ed->masters[ed->masters_count - 1] = a->lines[line];
+  /*
   if(ed->hosts == NULL){
     ed->hosts = calloc(1, sizeof(struct piece*));
     ed->hosts_count = 1;
@@ -343,100 +224,26 @@ static int arse_update_arrays(struct arse *a, struct arse *ed, size_t line){
       return 2;
     }
   }
+  */
   return 0;
 }
-int arse_piece_to_arse(struct arse *a, size_t line, size_t index, size_t length, bool unique){
-  struct part *p = table_get_pieces(a->lines[line], index, length); 
-  struct arse *ed = arse_find_arse(a, line, length, p);
-  struct piece *new = piece_create(0,length,ARSE_EDITOR);
-  int ret = arse_update_arrays(a, ed, line);
-  if(ret != 0){
-    return ret;
-  }
-  ed->hosts[ed->hosts_count - 1] = new;
-  new->arse = ed;
-  new->master = a->lines[line];
-  table_replace_pieces(a->lines[line], p->first, p->last, new);
-  part_delete(p, false);
+int arse_piece_to_arse(struct arse *a, size_t index, size_t length, bool unique){
   return 0;
 }
 
-void arse_buffer_delete(struct arse_buffer *a){
-  for(int i = 0; i < a->lines_count; ++i){
-    free(a->lines[i]);
-  }
-  free(a->lines);
-  free(a->line_lengths);
-  free(a);
-}
 void arse_string_delete(struct arse_string *a){
   free(a->string);
   free(a);
 }
-size_t arse_index_for_line(struct arse *a, size_t line, size_t index){
-  size_t table = 0;
-  if(table_state_update(a->lines[table]) != 0){
-    debug("Failed to update table state\n");
-    return 1;
-  }
-
-  struct table_info state = a->lines[table]->state;
-  size_t table_line = 0;
-  size_t passed_lines = 0;
-  size_t len = 0;
-  while(passed_lines < line){
-    len += state.line_lengths[table_line++];
-    ++passed_lines;
-    if(table_line >= state.line_count){
-      /* Still on same line as before */
-      char *str = table_buffer(a->lines[table]);
-      if(str[a->lines[table]->length - 1] != '\n'){
-        --passed_lines;
-      }
-      free(str);
-      if(table_state_update(a->lines[++table]) != 0){
-        return 1;
-      }
-      state = a->lines[table]->state;
-      table_line = 0;
-    }
-  }
-  if(index > a->lines[table]->length){
-    index = a->lines[table]->length;
-  }
-  index = index + len;
-  return index;
-}
 static void parse_input_string(struct arse *a, char *input, size_t length){
-  //if(a->input_string != NULL){
-  //  free(a->input_string);
-  //}
-  //a->input_string = input;
+  a->buffers[ARSE_ORIGINAL] = input;
+  struct piece *p = piece_create(0, length, ARSE_ORIGINAL);
+
+  a->begin->next = p;
+  a->end->previous = p;
+
+  p->next = a->end;
+  p->previous = a->begin;
+
   debug("string given: %s\n", input);
-  //input = malloc(strlen(input));
-  //strcpy(a->input_string, input);
-  a->lines_count = linecount_get(input);
-  a->lines = calloc(a->lines_count, sizeof(struct table*));
-  int i = 0;
-  char *start, *end, *strend = input + length;
-  start = end = input;
-  while(end != strend){
-    end = strchr(start, '\n');
-    /* Control for single line input */
-    if(end == NULL){
-      end = strend;
-    }
-    /* length + newline */
-    size_t len = end - start + 1;
-    char *token = calloc(len + 1, sizeof(char));
-    strncpy(token, start, len);
-    token[len] = 0;
-    a->lines[i++] = table_create(token, false);
-    start = end + 1;
-  }
-  if(a->lines_count > i){
-    --a->lines_count;
-  }
-  free(input);
-  a->absolute_lines_count = a->lines_count;
 }
--- src/arse.h
+++ src/arse.h
@@ -27,43 +27,32 @@ struct arse_string{
   size_t length;
 };
 
-/// @brief All lines handled by arse instance with meta-information
-struct arse_buffer{
-  char **lines;
-  size_t *line_lengths;
-  size_t lines_count;
-};
-
 /// @brief Main editor and sub-editor structure
 struct arse{
-  /* TODO: some structure for keeping track of extra lines inside subarses */
-  struct table **lines;
-  struct table **masters;
-  struct piece **hosts;
-  struct table_stack *action_history;
-  struct table_stack *action_future;
-  struct subarse_table *slaves;
-  size_t lines_count;
-  size_t absolute_lines_count;
-  size_t masters_count;
-  size_t hosts_count;
-  char *filename;
-  char *input_string;
-  FILE *fp;
+  // Not entirely clear what this is tracking, seems like hosts[i]->master? But haven't found assignment for that
+  //struct table **masters;
+  //size_t masters_count;
+  // Host pieces that are referring to this as a sub-editor
+  //struct piece **hosts;
+  //struct table_stack *action_history;
+  //struct table_stack *action_future;
+  //struct subarse_table *slaves;
+  //size_t hosts_count;
+  //char *filename;
+  char *buffers[2]; // buffertype ARSE_ORIGINAL and ARSE_CHANGE
+  struct piece *begin;
+  struct piece *end;
+  //size_t length;
+  //struct table_info state;
+
+  //struct part_stack *history;
+  //struct part_stack *future;
 };
 
 /// @brief Initialise an arse instance at supplied memory space
 ///
 /// @param a Memory address to initialise
 void arse_init(struct arse *a);
-
-/// @brief Open and load content from file into arse instance
-///
-/// @param a Arse instance
-/// @param path Path to file
-///
-/// @return Zero on success, non-zero on failure
-int arse_open_file(struct arse *a, char *path);
 
 /// @brief Load arse instance with a basic string
 ///
@@ -85,16 +74,6 @@ void arse_delete(struct arse *a);
 /// @return Zero on success, non-zero on failure
 int arse_insert(struct arse *a, size_t index, char *str);
 
-/// @brief Insert string at index on line
-///
-/// @param a Arse instance
-/// @param line Line to insert in
-/// @param index Character index at specificed line
-/// @param str String to insert
-///
-/// @return Zero on success, non-zero on failure
-int arse_insert_at_line(struct arse *a, size_t line, size_t index, char *str);
-
 /// @brief Remove continuous sequence of character
 ///
 /// @param a Arse instance
@@ -104,22 +83,6 @@ int arse_insert_at_line(struct arse *a, size_t line, size_t index, char *str);
 /// @return Zero on success, non-zero on failure
 int arse_remove(struct arse *a, size_t index, size_t length);
 
-/// @brief Remove continuous sequence of character at line
-///
-/// @param a Arse instance
-/// @param line Line to remove from
-/// @param index Index to start removal from at specified line
-/// @param length Length of sequence
-///
-/// @return Zero on success, non-zero on failure
-int arse_remove_at_line(struct arse *a, size_t line, size_t index, size_t length);
-
-/// @brief Create a new, empty line
-///
-/// @param a Arse instance
-/// @param line Position to add line at
-void arse_new_line(struct arse *a, size_t line);
-
 /// @brief Undo last action
 ///
 /// This will remove the last action of the most recently modified
@@ -127,15 +90,6 @@ void arse_new_line(struct arse *a, size_t line);
 ///
 /// @param a Arse instance
 void arse_undo(struct arse *a);
-
-/// @brief Get absolute index (starting at 0 from first line)
-///
-/// @param a Arse instance
-/// @param line Line which index is relative to
-/// @param index Relative index to line
-///
-/// @return Absolute index
-size_t arse_index_for_line(struct arse *a, size_t line, size_t index);
 
 /// @brief Redo last action
 ///
@@ -145,30 +99,6 @@ size_t arse_index_for_line(struct arse *a, size_t line, size_t index);
 /// @param a Arse instance
 void arse_redo(struct arse *a);
 
-/// @brief Undo specific line
-///
-/// Undos the last action on specific line
-///
-/// @param a Arse instance
-/// @param line Line to undo
-void arse_undo_line(struct arse *a, size_t line);
-
-/// @brief Redo specific line
-///
-/// Redo the last undone action on specific line
-///
-/// @param a Arse instance
-/// @param line Line to redo
-void arse_redo_line(struct arse *a, size_t line);
-
-/// @brief Get string of specific line
-///
-/// @param a Arse instance
-/// @param line Line to fetch string from
-///
-/// @return String found at given line index
-char *arse_get_line(struct arse *a, size_t line);
-
 /// @brief Convert piece sequence for text sequence into a sub-editor arse instance
 ///
 /// @param a Main arse instance
@@ -177,38 +107,18 @@ char *arse_get_line(struct arse *a, size_t line);
 /// @param length Length of text sequence
 /// @param unique Force creation of new instance if duplicate is found, instead of sharing same instance
 ///
-/// @return 
-int arse_piece_to_arse(struct arse *a, size_t line, size_t index, size_t length, bool unique);
-
-/// @brief Write buffer to file
-///
-/// @param a Arse instance
-/// @param filename Path to target file
-///
-/// @return Zero on success
-int arse_save(struct arse *a, char *filename);
+/// @return Zero on success, non-zero on failure
+int arse_piece_to_arse(struct arse *a, size_t index, size_t length, bool unique);
 
 /// @brief Save backup file
 ///
 /// @param a Arse instance
 void arse_backup(struct arse *a);
 
-/// @brief Clean up arse buffer structure
-///
-/// @param a Arse buffer instance
-void arse_buffer_delete(struct arse_buffer *a);
-
 /// @brief Clean up arse string structure
 ///
 /// @param a Arse string instance
 void arse_string_delete(struct arse_string *a);
-
-/// @brief Get buffer information of arse instance
-///
-/// @param a Arse instance
-///
-/// @return Arse buffer structure containing buffer information
-struct arse_buffer *arse_get_buffer(struct arse *a);
 
 /// @brief Get plain string managed by arse instance
 ///
